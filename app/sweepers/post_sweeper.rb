41143701e5d5fc2b65f4606b6d2815b7186f7759
Chalo Fernandez <chalofa@gmail.com>
2012-02-23 14:59:30 -0600

    Remove sweeper code, let the implementation to be done in the container app

--- app/sweepers/post_sweeper.rb
+++ app/sweepers/post_sweeper.rb
@@ -1,26 +1,5 @@
 class PostSweeper < ActionController::Caching::Sweeper
   observe CrowdblogCore::Post
 
-  def after_create(post)
-    expire_all(post)
-  end
-
-  def after_update(post)
-    expire_all(post)
-  end
-
-  def after_destroy(post)
-    expire_all(post)
-  end
-
-  private
-  def expire_all(post)
-    if post.published?
-      expire_page(controller: '/posts', action: 'index')
-      expire_page(controller: '/posts', action: 'show',
-                  year: post.year, month: post.month, day: post.day, id: post.permalink, format: 'html')
-      expire_page atom_feed_path(format: 'xml')
-    end
-    expire_page preview_path(post.to_param)
-  end
+  # Implement it in your base app!
 end

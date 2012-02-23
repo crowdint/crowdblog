70a51df9d20585aba98fbaaec36789c26d324db2
Chalo Fernandez <chalofa@gmail.com>
2012-02-23 15:48:18 -0600

    Authentication method should use Devise first

--- app/controllers/crowdblog_core/application_controller.rb
+++ app/controllers/crowdblog_core/application_controller.rb
@@ -3,8 +3,10 @@ module CrowdblogCore
     before_filter :authenticate!
 
     def authenticate!
-      current_user = User.find_by_email('foo@crowdint.com') || User.create!(email: 'foo@crowdint.com', is_publisher: true)
-      sign_in current_user
+      unless user_signed_in?
+        current_user = User.find_by_email('foo@crowdint.com') || User.create!(email: 'foo@crowdint.com', is_publisher: true)
+        sign_in current_user
+      end
 
       #TODO: fix OAuth
       #redirect_to user_omniauth_authorize_path(:google_apps) unless current_user

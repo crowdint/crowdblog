29e48d2d1103d2c8d916631efdbd5cc1092bb0f2
Chalo Fernandez <chalofa@gmail.com>
2012-02-23 15:48:30 -0600

    Index steps for Cucumber

---
+++ features/step_definitions/index_steps.rb
@@ -0,0 +1,16 @@
+Given /^the following posts by "([^"]*)":$/ do |name, table|
+  author = CrowdblogCore::User.find_by_name name
+  table.hashes.each_with_index do |hash, index|
+    time_index = (index * 5)
+    p = CrowdblogCore::Post.create!(title: hash[:title], body: hash[:body])
+    p.author = author
+    p.regenerate_permalink
+    p.save!
+    p.publish if hash[:published] == 'true'
+    p.update_attribute(:published_at, time_index.days.ago)
+  end
+end
+
+When /^I go to the index page$/ do
+  visit root_path
+end

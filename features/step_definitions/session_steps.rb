cb831062f9cbec3883f6120b3958fcfb63eaeefb
Chalo Fernandez <chalofa@gmail.com>
2012-02-23 23:59:15 -0600

    Make sure the auth_token is never nil in Cucumber tests

--- features/step_definitions/session_steps.rb
+++ features/step_definitions/session_steps.rb
@@ -3,6 +3,7 @@ include Devise::TestHelpers
 Given /^I am signed in as "([^"]*)"$/ do |email|
   @current_user = Crowdblog::User.create!(email: email)
   @current_user.reset_authentication_token!
+  @current_user.authentication_token.should_not be_nil
   visit crowdblog_path + "?auth_token=#{@current_user.authentication_token}"
 end
 

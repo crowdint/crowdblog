fd1edf6a6cbf58140f9df167097606355cec2ba4
Chalo Fernandez <chalofa@gmail.com>
2012-02-25 02:13:56 -0600

    User and Post fabricators

---
+++ spec/fabricators/posts_fabricator.rb
@@ -0,0 +1,21 @@
+Fabricator :post, class_name: 'Crowdblog::Post' do
+  title                 { Faker::Lorem.sentence }
+  body                  { Faker::Lorem.words(rand(10) + 1) }
+  state                 'drafted'
+  author(fabricator: :user)
+end
+
+Fabricator :post_test, from: :post do
+  title                 'This is a Test Post'
+  body                  'A great post about how Crowd Interactive ROCKS!'
+  author(fabricator: :user_test)
+end
+
+Fabricator :post_published, from: :post do
+  title                 'This is a Published Post'
+  body                  'Published Post! *yeeei*'
+  state                 'published'
+  published_at          { 1.minute.ago }
+  publisher(fabricator: :user_publisher)
+  permalink             { |post| post.title.parameterize }
+end
---
+++ spec/fabricators/users_fabricator.rb
@@ -0,0 +1,19 @@
+Fabricator :user, class_name: 'Crowdblog::User' do
+  email                 { sequence(:email) { |i| "user#{i}@crowdint.com" } }
+  name                  { Faker::Name.name }
+  is_publisher          false
+end
+
+Fabricator :user_test, from: :user do
+  email                 'test@crowdint.com'
+  name                  'Test User'
+  is_publisher          false
+  authentication_token  'user'
+end
+
+Fabricator :user_publisher, from: :user do
+  email                 'publisher@crowdint.com'
+  name                  'Publisher User'
+  is_publisher          true
+  authentication_token  'publisher'
+end

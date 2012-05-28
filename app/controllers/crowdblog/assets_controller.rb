d9a87a96e239d3b824ba30dee5010b0fe347fd5b
David Padilla <david@crowdint.com>
2012-05-28 16:59:21 -0500

    Fix the uploadify issue in the worst possible way I could

--- app/controllers/crowdblog/assets_controller.rb
+++ app/controllers/crowdblog/assets_controller.rb
@@ -1,5 +1,9 @@
 module  Crowdblog
   class AssetsController < Controller
+    # TODO: Skipping filters is the worst solution ever to this problem
+    # Someone should fix the uploadify.js thing
+    skip_before_filter :verify_authenticity_token, :only => :create
+    skip_before_filter :authorize!
 
     def create
       @post = Post.find(params[:post_id])

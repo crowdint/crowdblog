1f82d57663449443b93f979ff3af16ff74bfdd22
David Padilla <david@crowdint.com>
2012-10-07 16:11:45 -0500

    Refactor the capybara specs

--- lib/crowdblog/rspec/crowdblog_shared_examples.rb
+++ lib/crowdblog/rspec/crowdblog_shared_examples.rb
@@ -23,86 +23,85 @@ shared_examples_for "a crowdblog", :type => :integration do
       click_link 'New Post'
 
       fill_in 'Title', :with => 'A post title'
-      fill_in 'Body', :with => 'A post body'
+      fill_in 'Body' , :with => 'A post body'
       click_button 'Update'
 
       page.current_path.should == crowdblog.posts_path
       page.should have_content 'A post title'
     end
 
-    it "edits a post" do
-      post
-      post.author = user
-      post.save!
-
-      visit crowdblog.posts_path
-      within "#post_#{post.id}" do
-        click_link 'Edit'
+    context "a post exists" do
+      before do
+        post
+        post.author = user
+        post.save!
       end
 
-      fill_in 'Title', :with => 'A NEW post title'
-      fill_in 'Body', :with => 'A NEW post body'
-      click_button 'Update'
+      it "edits a post" do
+        visit crowdblog.posts_path
+        within "#post_#{post.id}" do
+          click_link 'Edit'
+        end
 
-      page.current_path.should == crowdblog.posts_path
-      page.should have_content 'A NEW post title'
-    end
+        fill_in 'Title', :with => 'A NEW post title'
+        fill_in 'Body' , :with => 'A NEW post body'
+        click_button 'Update'
 
-    it "deletes a post" do
-      post
-      post.author = user
-      post.save!
-
-      visit crowdblog.posts_path
-
-      within "#post_#{post.id}" do
-        click_link 'Delete'
+        page.current_path.should == crowdblog.posts_path
+        page.should have_content 'A NEW post title'
       end
-      page.current_path.should == crowdblog.posts_path
-      page.should_not have_content 'A post title'
-    end
 
-    it "publishes a post", :js => true do
-      user.publisher!
+      it "deletes a post" do
+        visit crowdblog.posts_path
 
-      post
-      post.author = user
-      post.save!
-
-      visit crowdblog.posts_path
-
-      within "#post_#{post.id}" do
-        button = find_link 'Publish'
-        button.click
-
-        button[:class].should match /btn-success/
-        post.reload.state.should eq 'published'
-
-        button.click
-        button[:class].should match /btn-danger/
-        post.reload.state.should eq 'drafted'
+        within "#post_#{post.id}" do
+          click_link 'Delete'
+        end
+        page.current_path.should == crowdblog.posts_path
+        page.should_not have_content 'A post title'
       end
-    end
 
-    it "marks the post for review", :js => true do
-      user.publisher!
+      it "publishes a post", :js => true do
+        user.publisher!
 
-      post
-      post.author = user
-      post.save!
+        visit crowdblog.posts_path
 
-      visit crowdblog.posts_path
+        within "#post_#{post.id}" do
+          button = find_link 'Publish'
+          button.click
 
-      within "#post_#{post.id}" do
-        button = find_link 'Review'
-        button.click
+          page.should have_css 'a.btn-success'
+          post.reload.state.should eq 'published'
+        end
+      end
 
-        button[:class].should match /btn-warning/
-        post.reload.ready_for_review.should be_true
+      it "draftes a post", :js => true do
+        user.publisher!
+        post.publish!
 
-        button.click
-        button[:class].should_not match /btn-warning/
-        post.reload.ready_for_review.should be_false
+        visit crowdblog.posts_path
+
+        within "#post_#{post.id}" do
+          button = find_link 'Publish'
+          button.click
+
+          page.should have_css 'a.btn-danger'
+          post.reload.state.should eq 'drafted'
+        end
+      end
+
+      it "marks the post for review", :js => true do
+        user.publisher!
+
+        visit crowdblog.posts_path
+
+        within "#post_#{post.id}" do
+          button = find_link 'Review'
+          button.click
+
+          page.should have_css 'a.btn-warning'
+          post.reload.ready_for_review.should be_true
+        end
       end
     end
   end

b6162fbe63a1db2e197454f894c7232a8d2aaaeb
David Padilla <david@crowdint.com>
2012-10-07 23:00:42 -0500

    Initialize assets

--- lib/crowdblog/engine.rb
+++ lib/crowdblog/engine.rb
@@ -1,5 +1,9 @@
 module Crowdblog
   class Engine < ::Rails::Engine
     isolate_namespace Crowdblog
+
+    initializer :assets do |config|
+      Rails.application.config.assets.precompile += %w( crowdblog.css crowdblog.js )
+    end
   end
 end

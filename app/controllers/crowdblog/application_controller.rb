89e9840f2b4c19854975d27233f7a725799d00d9
David Padilla <david@crowdint.com>
2012-10-11 20:09:20 -0500

    current_user is now method missing anything that overrides it gains priority

--- app/controllers/crowdblog/application_controller.rb
+++ app/controllers/crowdblog/application_controller.rb
@@ -1,7 +1,10 @@
 module Crowdblog
   class ApplicationController < ActionController::Base
-    def current_user
-      User.new
+    def method_missing(method_name)
+      if method_name == :current_user
+        Rails.logger.warn("current_user in Crowdblog::ApplicationController should be overriden")
+        User.new
+      end
     end
 
     helper_method :current_user

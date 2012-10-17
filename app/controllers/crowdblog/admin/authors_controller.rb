46715dddb0ff2f7158d3f37d27669d0f66f8983b
David Padilla <david@crowdint.com>
2012-10-17 11:39:08 -0500

    Admin Authors module should not be part of the gem

--- app/controllers/crowdblog/admin/authors_controller.rb
+++
@@ -1,9 +0,0 @@
-module  Crowdblog
-  module Admin
-    class AuthorsController < Crowdblog::Admin::BaseController
-      def index
-        @authors = User.includes(:published_posts).sort {|a,b| b.published_posts.size <=> a.published_posts.size}
-      end
-    end
-  end
-end

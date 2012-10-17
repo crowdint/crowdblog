fd9036eecc078041e55b0d3c2a6d4fcbe87e6c0d
David Padilla <david@crowdint.com>
2012-10-17 12:03:43 -0500

    Class variables to set the user class

--- lib/crowdblog.rb
+++ lib/crowdblog.rb
@@ -9,3 +9,11 @@ require "state_machine"
 require "strong_parameters"
 
 require "generators/crowdblog/views_generator"
+
+module Crowdblog
+  @@author_user_class_name    = 'User'
+  @@publisher_user_class_name = 'User'
+
+  mattr_accessor :author_user_class_name
+  mattr_accessor :publisher_user_class_name
+end

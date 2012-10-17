9cbf734673b6663dae5c4db1bbe5e24121d23d3f
David Padilla <david@crowdint.com>
2012-10-17 11:03:40 -0500

    Crowdblog Application controller should inherit from ::ApplicationController

--- app/controllers/crowdblog/application_controller.rb
+++ app/controllers/crowdblog/application_controller.rb
@@ -1,5 +1,5 @@
 module Crowdblog
-  class ApplicationController < ActionController::Base
+  class ApplicationController < ::ApplicationController
     def method_missing(method_name)
       if method_name == :current_user
         Rails.logger.warn("current_user in Crowdblog::ApplicationController should be overriden")

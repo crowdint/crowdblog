3175f336c1c03ed0dd2cbd9698143cbf4e38ec4a
David Padilla <david@crowdint.com>
2013-02-26 12:45:47 -0800

    Fix an issue where state_machine is not setting the initial state

--- lib/crowdblog/rspec/crowdblog_shared_examples.rb
+++ lib/crowdblog/rspec/crowdblog_shared_examples.rb
@@ -1,7 +1,7 @@
 shared_examples_for "a crowdblog", :type => :feature do
 
   let(:post) do
-    Crowdblog::Post.create :title => 'A post title', :body => 'A post body'
+    Crowdblog::Post.create :title => 'A post title', :body => 'A post body', :state => :drafted
   end
 
   describe "Home" do

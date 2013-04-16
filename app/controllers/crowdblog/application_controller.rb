73df2f7130d2ce36fbafe15a93f23e11290667a2
Fernando Perales & Gilberto Villa <pair+fer.perales+gilberto.villa@crowdint.com>
2013-04-16 18:01:24 -0500

    Extract wrappers to host application

--- app/controllers/crowdblog/application_controller.rb
+++ app/controllers/crowdblog/application_controller.rb
@@ -1,14 +1,5 @@
 module Crowdblog
    class ApplicationController < ::ApplicationController
 
-    helper_method :crowdblog_current_user, :crowdblog_authenticate_user!
-    
-    def crowdblog_current_user
-      current_user
-    end
-
-    def crowdblog_authenticate_user!
-      authenticate_user!
-    end
   end
 end
--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -6,4 +6,14 @@ class ApplicationController < ActionController::Base
   def current_user
     User.last
   end
+
+  def crowdblog_current_user
+    current_user
+  end
+
+  def crowdblog_authenticate_user!
+    authenticate_user!
+  end
+
+  helper_method :crowdblog_current_user, :crowdblog_authenticate_user!
 end
